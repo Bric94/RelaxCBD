98efc733effaaf970fbcdcb059eae03c93758e90
06011-39-01 <06011-39-01@exchange.ad.afpanet>
2025-03-05 13:25:36 +0100

    mailer

---
+++ migrations/Version20250305105759.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250305105759 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE contact ADD last_name VARCHAR(255) NOT NULL, ADD object VARCHAR(255) NOT NULL, ADD date DATE NOT NULL, DROP created_at, CHANGE name first_name VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE contact ADD name VARCHAR(255) NOT NULL, ADD created_at DATETIME NOT NULL, DROP first_name, DROP last_name, DROP object, DROP date');
+    }
+}
