05724112a6700a57fd721ba0241a125f7f86b785
Bric94 <eliottbricout@yahoo.fr>
2025-05-12 13:48:00 +0200

    cookies et sessions

--- public/index.php
+++ public/index.php
@@ -2,7 +2,10 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+ini_set('session.gc_maxlifetime', 31536000);
+ini_set('session.cookie_lifetime', 31536000);
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
